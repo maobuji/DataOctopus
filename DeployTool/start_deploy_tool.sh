33dcbf0d9822fcee89ac7776eb4f14c7aa55c16d
16770864@qq.com <16770864@qq.com>
2017-08-28 18:00:05 +0800

    添加kafka样例

---
+++ DeployTool/start_deploy_tool.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+PATH="$PATH:./product-config/apache-ant-1.8.4/bin/";
+export PATH;
+
+export ANT_HOME="./product-config/apache-ant-1.8.4";
+ant;
---
+++ DeployTool/startant.cmd
@@ -0,0 +1,4 @@
+set PATH=./product-config/apache-ant-1.8.4/bin/;%PATH%
+set ANT_HOME=./product-config/apache-ant-1.8.4
+
+ant

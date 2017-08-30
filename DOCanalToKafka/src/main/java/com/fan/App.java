d2d72e128805622a90564a63d2ef308fa10824cb
16770864@qq.com <16770864@qq.com>
2017-08-30 18:03:33 +0800

    加入读取canal的工程和kafka内容，准备将数据写入kafka中

---
+++ DOCanalToKafka/src/main/java/com/fan/App.java
@@ -0,0 +1,98 @@
+package com.fan;
+
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.common.utils.AddressUtils;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.alibaba.otter.canal.protocol.Message;
+import org.jetbrains.annotations.NotNull;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+/**
+ * Created by fanmingming on 2017/8/30.
+ */
+public class App {
+
+    public static void main(String args[]) {
+        // 创建链接
+        CanalConnector connector = CanalConnectors
+                .newSingleConnector(new InetSocketAddress(AddressUtils.getHostIp(), 11111), "example", "", "");
+        int batchSize = 1000;
+
+        try {
+            connector.connect();
+            connector.subscribe(".*\\..*");
+            connector.rollback();
+            int totalEmtryCount = 1200;
+            while (true) {
+                Message message = connector.getWithoutAck(batchSize); // 获取指定数量的数据
+                long batchId = message.getId();
+                int size = message.getEntries().size();
+                if (batchId == -1 || size == 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    printEntry(message.getEntries());
+                }
+
+                connector.ack(batchId); // 提交确认
+                // connector.rollback(batchId); // 处理失败, 回滚数据
+            }
+
+            System.out.println("empty too many times, exit");
+        } finally {
+            connector.disconnect();
+        }
+    }
+
+    private static void printEntry(@NotNull List<CanalEntry.Entry> entrys) {
+        for (CanalEntry.Entry entry : entrys) {
+            if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN
+                    || entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
+
+                System.out.println("trans:"+entry.getEntryType().toString()+" m="+entry.getSerializedSize());
+                continue;
+            }
+
+            CanalEntry.RowChange rowChage = null;
+            try {
+                rowChage = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+            } catch (Exception e) {
+                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(),
+                        e);
+            }
+
+            CanalEntry.EventType eventType = rowChage.getEventType();
+            System.out.println(String.format("================> binlog[%s:%s] , name[%s,%s] , eventType : %s,version : %s,entrytype : %s",
+                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(), eventType,entry.getHeader().getExecuteTime(),entry.getEntryType().toString()));
+
+
+            for (CanalEntry.RowData rowData : rowChage.getRowDatasList()) {
+                if (eventType == CanalEntry.EventType.DELETE) {
+                    printColumn(rowData.getBeforeColumnsList());
+                } else if (eventType == CanalEntry.EventType.INSERT) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else {
+                    System.out.println("-------> before");
+                    printColumn(rowData.getBeforeColumnsList());
+                    System.out.println("-------> after");
+                    printColumn(rowData.getAfterColumnsList());
+                }
+            }
+        }
+    }
+
+
+    private static void printColumn(@NotNull List<CanalEntry.Column> columns) {
+        for (CanalEntry.Column column : columns) {
+
+            System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
+        }
+    }
+}
--- DeployTool/startAll.bat
+++ DeployTool/startAll.bat
@@ -3,9 +3,18 @@ SET home=%cd%
 cd  %home%\mysql\bin
 start mysqld --console
 
-ping 127.0.0.1 -n 5 >nul
+ping 127.0.0.1 -n 10 >nul
 cd  %home%\canal\bin
 start startup.bat
+
+ping 127.0.0.1 -n 5 >nul
+cd  %home%\kafka\bin\windows
+start zookeeper-server-start.bat ../../config/zookeeper.properties
+ping 127.0.0.1 -n 10 >nul
+
+cd  %home%\kafka\bin\windows
+start kafka-server-start.bat ../../config/server.properties
+
 
 
 
